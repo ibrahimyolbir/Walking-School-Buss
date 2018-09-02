a8a23b37def871546394604265b52a0508c0d035
System Administrator <root@Caners-MacBook-Air.local>
2018-09-02 03:16:34 +0200

    login changes

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-const login = document.querySelector('.login');
-const signup = document.querySelector('.signup');
-const formLogin = document.querySelector('.login-form');
-const formSignup = document.querySelector('.signup-form');
 
-function loginForm() {
-	login.classList.add('active');
-	login.classList.remove('inactive');
-	signup.classList.add('inactive');
-	signup.classList.remove('active');
-	formLogin.classList.remove('hide');
-	formSignup.classList.add('hide');
-}
-
-function signupForm() {
-	login.classList.add('inactive');
-	login.classList.remove('active');
-	signup.classList.add('active');
-	signup.classList.remove('inactive');
-	formLogin.classList.add('hide');
-	formSignup.classList.remove('hide');
-}
-
-login.addEventListener('click', loginForm);
-signup.addEventListener('click', signupForm);
+jQuery(document).ready(function($){
+	$(".dropdown-button").dropdown();
+	$('.modal').modal();
+	$(".signup-toggle").click(function(){
+		$(this).hide();
+		$(".signupForm").show(300);
+		$(".policy").css("visibility","visible");
+	});
+});
