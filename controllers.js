2e915d9fc3f42292b25dfcabfc3b92662c688a9d
ibrahim yolbir <ibrahimyolbirr@gmail.com>
2018-09-15 15:39:30 +0200

    Test commit

--- controllers.js
+++ controllers.js
@@ -1,7 +1,48 @@
 var app = angular.module('wsbApp');
 
 app.controller('LoginController', ['$scope','$http', function($scope,$http){
-    console.log('LoginController');
+
+jQuery(document).ready(function($){
+    $('.signup').hide();
+    $('.recover-password').hide();
+    
+    
+    $('.btn-reset').click(function () {
+        $('.login').hide();
+        $('.recover-password').fadeIn(300);
+    });
+    
+    $('.btn-member').click(function () {
+        $('.login').hide();
+        $('.signup').fadeIn(300);
+    });
+    
+    
+    $('.btn-login').click(function () {
+        $('.signup').hide();
+        $('.recover-password').hide();
+        $('.login').fadeIn(300);
+    
+    });
+    
+    $('.notification').hide();
+    
+    $('.btn-password').click(function () {
+    
+        if($('#resetPassword').val()==0) {
+            // $('#resetPassword').after('<span class="error">Email not valid.</span>')
+            $('.error').text('Email not valid.')
+        }
+    
+       else {
+            $('.reset-mail').text($('#resetPassword').val());
+            $('.recover-password form').hide();
+            $('.notification').fadeIn(300);
+        }
+    });
+    
+});
+ 
 
 }]);
 app.controller('HomeController', ['$scope','$http', function($scope,$http){
--- main.js
+++ main.js
@@ -1,50 +1,11 @@
+ 
 
-jQuery(document).ready(function($){
-    $('.signup').hide();
-    $('.recover-password').hide();
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
     
-    
-    $('.btn-reset').click(function () {
-        $('.login').hide();
-        $('.recover-password').fadeIn(300);
-    });
-    
-    $('.btn-member').click(function () {
-        $('.login').hide();
-        $('.signup').fadeIn(300);
-    });
-    
-    
-    $('.btn-login').click(function () {
-        $('.signup').hide();
-        $('.recover-password').hide();
-        $('.login').fadeIn(300);
-    
-    });
-    
-    $('.notification').hide();
-    
-    $('.btn-password').click(function () {
-    
-        if($('#resetPassword').val()==0) {
-            // $('#resetPassword').after('<span class="error">Email not valid.</span>')
-            $('.error').text('Email not valid.')
-        }
-    
-       else {
-            $('.reset-mail').text($('#resetPassword').val());
-            $('.recover-password form').hide();
-            $('.notification').fadeIn(300);
-        }
-    });
-    
-});
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+ 
 
